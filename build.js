df9a45abc970484dde5b8629b16d653a03b63ade
Oscar <56079903+OscarNOW@users.noreply.github.com>
2024-07-26 17:14:59 +0200

    Added fake headers

--- build.js
+++ build.js
@@ -50,7 +50,29 @@ async function getFileFromExternalSource(provided) {
     for (const uri of possibleUris) {
         console.log(`Trying to load from ${uri}`);
         try {
-            const response = await fetch(uri);
+            // const response = await fetch(uri);
+            const response = fetch(uri, {
+                "headers": {
+                    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+                    "accept-language": "nl-NL,nl;q=0.9,en-US;q=0.8,en;q=0.7",
+                    "cache-control": "no-cache",
+                    "pragma": "no-cache",
+                    "priority": "u=0, i",
+                    "sec-ch-ua": "\"Not)A;Brand\";v=\"99\", \"Google Chrome\";v=\"127\", \"Chromium\";v=\"127\"",
+                    "sec-ch-ua-mobile": "?0",
+                    "sec-ch-ua-platform": "\"Windows\"",
+                    "sec-fetch-dest": "document",
+                    "sec-fetch-mode": "navigate",
+                    "sec-fetch-site": "cross-site",
+                    "sec-fetch-user": "?1",
+                    "upgrade-insecure-requests": "1",
+                    "Referer": "https://github.com/",
+                    "Referrer-Policy": "strict-origin-when-cross-origin"
+                },
+                "body": null,
+                "method": "GET"
+            });
+
             if (!`${response.status}`.startsWith('2')) {
                 console.log(`Status code is ${response.status}`);
                 continue;
