336c3937091d528e1dd535e6f7059338baaf381b
Oscar <56079903+OscarNOW@users.noreply.github.com>
2025-06-29 14:46:58 +0200

    Improved proxy

--- proxy.js
+++ proxy.js
@@ -1,3 +1,5 @@
+const port = 3001;
+
 const http = require('http');
 const httpProxy = require('http-proxy');
 const auth = require('basic-auth');
@@ -10,6 +12,9 @@ SECURE_ENABLED = SECURE_ENABLED === true || SECURE_ENABLED === 'true';
 
 if (SECURE_ENABLED && (!SECURE_USER || !SECURE_PASS))
     throw 'Missing SECURE_USER or SECURE_PASS. Please specify both in the environment variables.';
+
+if (!SECURE_ENABLED)
+    console.warn('Secure not enabled, the database is open to anyone that can access the url');
 
 const proxy = httpProxy.createProxyServer({});
 
@@ -25,6 +30,18 @@ http.createServer((req, res) => {
     }
 
     proxy.web(req, res, { target: 'http://localhost:3000' });
-}).listen(3001, () => {
-    console.log('Proxied Prisma Studio available at http://localhost:3001');
+}).listen(port, () => {
+    // to ensure this log is at bottom
+    setTimeout(log, 1000 * 2);
+    setTimeout(log, 1000 * 30);
 });
+
+function log() {
+    console.log('');
+    console.log(`Proxied Prisma Studio available at port ${port}`);
+    if (SECURE_ENABLED) {
+        console.log(`Username: ${SECURE_USER}`);
+        console.log(`Password: ${SECURE_PASS}`);
+    }
+    console.log('');
+}
